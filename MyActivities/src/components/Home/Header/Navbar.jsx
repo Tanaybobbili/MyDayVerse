ce760e569683f31586786cfe07c6658b6d2d4f33
Tanaybobbili <tanaybobbili.129@gmail.com>
2025-06-11 19:55:33 +0530

    Adjusted navbar and css styling.

--- MyActivities/src/components/Home/Header/Navbar.jsx
+++ MyActivities/src/components/Home/Header/Navbar.jsx
@@ -1,16 +1,24 @@
+import {useNavigate} from 'react-router-dom'
 function Navbar() {
+    const navigate = useNavigate();
     const handleMyActivityButton = () => {
-            
+        navigate('/myactivities');
+    }
+    const handleHomeButton = () => {
+        navigate('/');
     }
     return (<div className = "navbar">
-        <button className = "homebutton">
+        <button className = "homebutton" onClick={()=>{handleHomeButton()}}>
             Home
         </button>
-        <button className="activities-page-button" onClick={()=>{handleMyActivityButton}}>
+        <button className="activities-page-button" onClick={()=>{handleMyActivityButton()}}>
             My Activity List
         </button>
+        <div className="search-container"><input id="searchbar" className="searchbar" type="text" placeholder="Search" />
+            <label className="search-icon" htmlFor="searchbar"><i className="fa fa-search"></i></label>
+        </div>
+
         
-        <input className="searchbar"type="text" placeholder="Search"/>
         <button className="notificationsbutton">Notifications</button>
     </div>)
 }
