0d1a381ec3f7b0b5831f05062cc9618f3ddf1795
Tanaybobbili <tanaybobbili.129@gmail.com>
2025-06-14 23:05:54 +0530

    Initial handling of the cards in home page.

--- MyActivities/src/components/Home/Body/Body.jsx
+++ MyActivities/src/components/Home/Body/Body.jsx
@@ -1,23 +1,45 @@
-import { useEffect, useState } from 'react';
-import './Body.css';
+import { useNavigate } from 'react-router-dom';
+import './Body.css'; 
+import news from '../../../assets/news.png';
 
 function Body() {
-    const [time, setTime] = useState(new Date());
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const timer = setInterval(() => {
-            setTime(new Date());
-        }, 1000);
+  const cardData = [
+    {
+      title: 'Movies',
+      image: 'https://cdn-icons-png.flaticon.com/512/3163/3163478.png', 
+      link: '/movies',
+    },
+    {
+      title: 'Games',
+      image: 'https://cdn-icons-png.flaticon.com/512/4712/4712103.png', 
+      link: '/games',
+    },
+    {
+        title: 'News',
+        image: news,
+        link: '/news',
+    }
+  ];
 
-        return () => clearInterval(timer);
-    }, []);
-
-    return (
-        <div className="page">
-            <p>Hi</p>
-            <p className="time">{time.toLocaleString()}</p>
-        </div>
-    );
+  return (
+    <div className="home-section">
+      <h2 className="section-title">Explore Categories</h2>
+      <div className="card-container">
+        {cardData.map((card, index) => (
+          <div
+            className="custom-card"
+            key={index}
+            onClick={() => navigate(card.link)}
+          >
+            <img src={card.image} alt={card.title} className="card-image" />
+            <h3 className="card-title">{card.title}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Body;
